c97fbcd78f65ab502e7267a4ea9bfd422d53fb96
Goph <tealeverywhere@gmail.com>
2023-03-01 20:30:43 -1000

    email validation basic go

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+function checkEmail(){
+  //email
+  let email = document.forms['myForm']['email'];
+  if(email.value === ""){
+    //alert("Must have email value");
+    console.log('BLANK EMAIL');
+    email.setCustomValidity("invalid");
+    return false;
+  }
+  email.setCustomValidity("");
+  console.log("TRUE")
+  return true;
+}
